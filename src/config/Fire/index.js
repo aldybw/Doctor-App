577476f61d1742ce0075774a0a0cbd7c8be76f91
Aldycky1 <bagusaldycky@gmail.com>
2021-10-29 19:57:59 +0700

    first commit

---
+++ src/config/Fire/index.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyDhDDWnAPcbOk1nzKBR6YdF9VgkdC7A1Ig',
+  authDomain: 'my-doctor-ea249.firebaseapp.com',
+  databaseURL:
+    'https://my-doctor-ea249-default-rtdb.asia-southeast1.firebasedatabase.app/',
+  projectId: 'my-doctor-ea249',
+  storageBucket: 'my-doctor-ea249.appspot.com',
+  messagingSenderId: '712836017383',
+  appId: '1:712836017383:web:5076736ba8d9496ccf8642',
+});
+
+const Fire = firebase;
+
+export default Fire;
